4febf4a359d2cfdc1e751be9dd6e49a266d04cfb
yugo-ibuki <y.ibuki91@gmail.com>
2023-12-03 02:35:49 +0900

    add firebase

---
+++ src/libs/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STOREAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export { app, db, collection, addDoc, getDocs }
