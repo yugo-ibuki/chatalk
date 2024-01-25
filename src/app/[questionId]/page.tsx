27ec48d37443ab3f7e7ae1439c7284b621a93d86
Yugo Ibuki <y.ibuki91@gmail.com>
2024-01-26 01:24:24 +0900

    feat: form handler (#5)

--- src/app/[questionId]/page.tsx
+++ src/app/[questionId]/page.tsx
@@ -1,22 +1,43 @@
 'use client'
 
-import { Box, Input, Stack, Text } from '@chakra-ui/react'
+import { Box, Button, Input, Stack, Text } from '@chakra-ui/react'
 import { useQuestionAnsewerPage } from '@/hooks/useQuestionAnsewerPage'
 
 const Page = () => {
-  const { questions, error } = useQuestionAnsewerPage()
+  const { questions, error, onSubmit, form } = useQuestionAnsewerPage()
+  const { formState, register } = form
 
   if (error) return <div>エラーが発生しました</div>
+  console.log(form.formState.errors)
 
   return (
-    <Box margin={'30px auto'}>
+    <Box margin={'30px auto'} padding={'0 20px'}>
       <Stack width={'100%'} alignItems={'center'} justifyContent={'center'}>
-        {questions.map((question, index) => (
-          <Stack key={index} gap={3} margin={5}>
-            <Text>{question.question}</Text>
-            <Input />
-          </Stack>
-        ))}
+        <form onSubmit={onSubmit}>
+          {questions.map((question, index) => (
+            <Stack key={index} gap={3} marginY={5} width={'400px'}>
+              <Text>{question.question}</Text>
+              <Input
+                type="text"
+                id={'question' + index}
+                {...register('question' + index, {
+                  maxLength: {
+                    value: 100,
+                    message: '100文字以内で入力してください',
+                  },
+                })}
+              />
+              {/* エラー */}
+              {formState.errors['question' + index] ? (
+                // @ts-ignore
+                <Text color={'red'}>{formState.errors['question' + index]?.message}</Text>
+              ) : null}
+            </Stack>
+          ))}
+          <Button type="submit" marginY={5}>
+            回答する
+          </Button>
+        </form>
       </Stack>
     </Box>
   )
