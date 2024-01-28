7765bb8ed5b72f24b4df8fb0144e0bc7a900fade
yugo-ibuki <y.ibuki91@gmail.com>
2024-01-28 10:32:48 +0900

    chore: link to top

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import Link from 'next/link'
+
 const Header = () => {
   return (
     <header>
       <div className="border-b-2">
         <div className="py-3 mx-5">
-          <h1>Chatalk</h1>
+          <Link href={'/'}>
+            <h1>Chatalk</h1>
+          </Link>
         </div>
       </div>
     </header>
