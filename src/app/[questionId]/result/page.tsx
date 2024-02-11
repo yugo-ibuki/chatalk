3ec8fb78b597a7616d5a93721b3f93af252e0569
yugo-ibuki <y.ibuki91@gmail.com>
2024-02-12 00:12:05 +0900

    feat: add back button to result page to recreate questions

--- src/app/[questionId]/result/page.tsx
+++ src/app/[questionId]/result/page.tsx
@@ -3,6 +3,7 @@
 import { useResultPage } from '@/hooks/useResultPage'
 import { Box, Center, Spinner } from '@chakra-ui/react'
 import { AnswerRow } from '@/components/parts/AnswerRow'
+import Link from 'next/link'
 
 const Page = () => {
   const { answers, loading, isBothAnswered } = useResultPage()
@@ -16,15 +17,32 @@ const Page = () => {
 
   return (
     <Box px="3rem">
-      {isBothAnswered && answers?.questions
-        ? answers?.questions.map((answer, i) => (
+      {isBothAnswered && answers?.questions ? (
+        <Box>
+          {answers?.questions.map((answer, i) => (
             <AnswerRow
               key={answer.question + i}
               question={answer.question}
               answer={answer.answer}
             />
-          ))
-        : '回答が揃っていません。少し待ってから再度アクセスしてください。'}
+          ))}
+          <Box
+            sx={{
+              textAlign: 'center',
+              fontSize: '1rem',
+              border: '2px solid #4682b4',
+              borderRadius: '0.5rem',
+              width: 'fit-content',
+              padding: '0.5rem 1rem',
+              margin: '4rem auto 0',
+            }}
+          >
+            <Link href="/">もう一度やる</Link>
+          </Box>
+        </Box>
+      ) : (
+        '回答が揃っていません。少し待ってから再度アクセスしてください。'
+      )}
     </Box>
   )
 }
